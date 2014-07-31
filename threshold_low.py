a5cef27080ba487c51983288388154b85ece299e
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-07-30 23:30:55 -0400

    Added new scripts.

--- threshold_low.py
+++ threshold_low.py
@@ -8,13 +8,12 @@ angle = np.arccos(threshold)
 
 colors = [caps.normalize_color(c) for c in colorbrewer.Paired[5]]
 
-colors = [(1.0, 0.0, 0.0)]
-
 vectors = [np.array([0, 0, 1])]
 
 caps.make_base_sphere(seed=1)
 
-encoders, encoder_colors = caps.colored_encoders(
-    vectors, colors, threshold, num_neurons)
+caps.draw_encoders(num_neurons)
 
-caps.draw_encoders(encoders, colors=encoder_colors)
+for vector, color in zip(vectors, colors):
+    caps.draw_cap(vector, color, angle)
+    caps.draw_vector(vector, color)
